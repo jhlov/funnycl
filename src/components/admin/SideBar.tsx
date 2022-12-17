11c9b9c4406b03bf344c624c0e3ba4674f3745ae
jhlov <jhlovv@gmail.com>
2022-12-17 22:32:58 +0900

    메인메뉴 아이콘

--- src/components/admin/SideBar.tsx
+++ src/components/admin/SideBar.tsx
@@ -1,5 +1,20 @@
+import AppsIcon from "@mui/icons-material/Apps";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import "./SideBar.scss";
+
 const SideBar = () => {
-  return <div className="border-right">사이드바</div>;
+  return (
+    <div className="side-bar border-end p-3">
+      <div className="main-menu-button">
+        <AppsIcon fontSize="large" />
+        <div>게임</div>
+      </div>
+      <div className="main-menu-button">
+        <ArticleOutlinedIcon fontSize="large" />
+        <div>문제</div>
+      </div>
+    </div>
+  );
 };
 
 export { SideBar };
--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,12 @@
 import { Header } from "components/admin/Header";
 import { SideBar } from "components/admin/SideBar";
+import "./Admin.scss";
 
 const Admin = () => {
   return (
-    <div>
+    <div className="admin">
       <Header />
-      <div className="d-flex">
+      <div className="d-flex admin-body">
         <SideBar />
         <div>서브</div>
       </div>
