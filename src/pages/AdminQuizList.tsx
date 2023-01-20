4ed106b281e8e5e4e25f6c695a159fac860a5cd7
jhlov <jhlovv@gmail.com>
2023-01-20 15:35:53 +0900

    퀴즈 리스트 레이아웃 수정

--- src/pages/AdminQuizList.tsx
+++ src/pages/AdminQuizList.tsx
@@ -40,18 +40,18 @@ const AdminQuizList = () => {
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th style={{ width: "40px" }}>#</th>
-            <th style={{ width: "120px" }}>타입</th>
+            <th style={{ width: "40px", minWidth: "40px" }}>#</th>
+            <th style={{ width: "100px", minWidth: "100px" }}>타입</th>
             <th>문제 이름</th>
             <th>이미지</th>
-            <th style={{ width: "100px" }}>과목</th>
-            <th style={{ width: "120px" }}>학년</th>
-            <th style={{ width: "120px" }}>제시어</th>
-            <th style={{ width: "100px" }}>난이도</th>
-            <th style={{ width: "80px" }}>점수</th>
-            <th style={{ width: "120px" }}>문제유형</th>
-            <th style={{ width: "120px" }}>생성일</th>
-            <th style={{ width: "80px" }}>삭제</th>
+            <th style={{ width: "60px", minWidth: "60px" }}>과목</th>
+            <th style={{ width: "120px", minWidth: "120px" }}>학년</th>
+            <th style={{ width: "120px", minWidth: "120px" }}>제시어</th>
+            <th style={{ width: "80px", minWidth: "80px" }}>난이도</th>
+            <th style={{ width: "60px", minWidth: "60px" }}>점수</th>
+            <th style={{ width: "100px", minWidth: "100px" }}>문제유형</th>
+            <th style={{ width: "120px", minWidth: "120px" }}>생성일</th>
+            <th style={{ width: "60px", minWidth: "60px" }}>삭제</th>
           </tr>
         </thead>
         <tbody>
@@ -59,7 +59,9 @@ const AdminQuizList = () => {
             <tr key={item.id}>
               <td>{i + 1}</td>
               <td>{item.type}</td>
-              <td className="text-start">{item.title}</td>
+              <td className="text-start admin-quiz-list__title">
+                {item.title}
+              </td>
               <td>
                 {item.image && (
                   <img
