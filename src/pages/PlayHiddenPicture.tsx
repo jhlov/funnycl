e6e06818bb420ca2ef26682344b2bf145469fa76
jhlov <jhlovv@gmail.com>
2023-02-01 00:19:57 +0900

    배경 바뀌는 버그 수정

--- src/pages/PlayHiddenPicture.tsx
+++ src/pages/PlayHiddenPicture.tsx
@@ -1,16 +1,18 @@
 import { PlayHiddenPictureBoard } from "components/play/hidden-picture/PlayHiddenPictureBoard";
 import { PlayHiddenPictureScore } from "components/play/hidden-picture/PlayHiddenPictureScore";
+import { useMemo } from "react";
 import "./PlayHiddenPicture.scss";
 
 export const PlayHiddenPicture = () => {
+  const bgName = useMemo(() => {
+    return `${process.env.PUBLIC_URL}/img/bg/background_0${Math.ceil(
+      Math.random() * 4
+    )}.png`;
+  }, []);
+
   return (
     <div className="play-hidden-picture">
-      <img
-        className="play-hidden-picture__bg"
-        src={`${process.env.PUBLIC_URL}/img/bg/background_0${Math.ceil(
-          Math.random() * 4
-        )}.png`}
-      />
+      <img className="play-hidden-picture__bg" src={bgName} />
       <PlayHiddenPictureBoard />
       <PlayHiddenPictureScore />
     </div>
